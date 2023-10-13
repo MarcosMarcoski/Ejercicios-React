d3b08a2396c5d4099fd13b05269b08203789bfa2
Marcoski <marcosdediego@gmail.com>
2023-10-13 19:35:44 +0200

    ejercicio 29

--- vite-project/ejercicios/src/App.jsx
+++ vite-project/ejercicios/src/App.jsx
@@ -1,12 +1,14 @@
 import { AlertClock } from "./componentes/AlertClock";
+import { Counter } from "./componentes/Counter";
 import { Welcome } from "./componentes/Welcome";
 
 export function App() {
   return (
     <div>
       <Welcome name={"John"} age={16} />
+      <Counter/>
 
-      <AlertClock/>
+    
     </div>
   );
 }
