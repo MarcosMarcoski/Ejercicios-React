b85e9646dc3025e7d3aead0f90b872d40473e4aa
Marcoski <marcosdediego@gmail.com>
2023-10-13 21:12:39 +0200

    ejercicio 34

--- vite-project/ejercicios/src/componentes/Counter.jsx
+++ vite-project/ejercicios/src/componentes/Counter.jsx
@@ -1,8 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { CounterDisplay } from "./CounterDisplay";
 
-export function Counter({start = 0}) {
+export function Counter({ start = 0 }) {
   const [counter, setCounter] = useState(start);
+  useEffect(() => {
+    console.log("el valor es:", counter);
+  }, [counter]);
 
   function increment() {
     setCounter(counter + 1);
@@ -10,15 +13,13 @@ export function Counter({start = 0}) {
   function decrement() {
     setCounter(counter - 1);
   }
-  function reset () {
+  function reset() {
     setCounter(0);
   }
 
-
-
   return (
     <div>
-        <CounterDisplay counter={counter} />
+      <CounterDisplay counter={counter} />
       <button onClick={increment}>INCREMENT</button>
       <button onClick={decrement}>DECREMENT</button>
       <button onClick={reset}>RESET</button>
