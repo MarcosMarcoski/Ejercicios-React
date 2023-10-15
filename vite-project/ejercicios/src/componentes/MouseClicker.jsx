eb41e772c748200156898b6dd0c2fc7d9ce9fb91
Marcoski <marcosdediego@gmail.com>
2023-10-16 01:03:24 +0200

    ejercicio 38

--- vite-project/ejercicios/src/componentes/MouseClicker.jsx
+++ vite-project/ejercicios/src/componentes/MouseClicker.jsx
@@ -2,11 +2,16 @@ export function MouseClicker() {
   function handleButton(event) {
     console.log(event.currentTarget.name);
   }
+  function handleImage(event) {
+    console.log(event.currenTarget.img);
+  }
 
   return (
     <button name="one" onClick={handleButton}>
-      <img width={24} height={20} />
+      <img onClick={handleImage} width={24} height={20} />
       click me!
     </button>
   );
 }
+
+// sigue imprimiendo el name "one" porque la imagen está dentro del button
