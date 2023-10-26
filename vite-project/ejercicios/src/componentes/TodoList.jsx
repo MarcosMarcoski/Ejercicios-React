d2aae106111b6078c7970d87d6b9e59d5c2e64a2
Marcoski <marcosdediego@gmail.com>
2023-10-26 14:11:53 +0200

    ejercicio 59

--- vite-project/ejercicios/src/componentes/TodoList.jsx
+++ vite-project/ejercicios/src/componentes/TodoList.jsx
@@ -1,35 +1,43 @@
 import { useState } from "react";
 
 export function TodoList() {
-  const [items, setItems] = useState([]);
-  const [inputValue, setInputValue] = useState("");
-
-  const handleAddItem = () => {
-    if (inputValue.trim() !== "") {
-      setItems([...items, inputValue]);
-      setInputValue("");
-    }
-   
-  };
-  const handleReset = () => {
-    setItems([]); 
-};
-  return (
-    <div>
-      <ul>
-        {items.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+    const [items, setItems] = useState([]);
+    const [inputValue, setInputValue] = useState("");
+  
+    const handleAddItem = () => {
+      if (inputValue.trim() !== "") {
+        setItems([...items, inputValue]);
+        setInputValue("");
+      }
+    };
+    const handleReset = () => {
+      setItems([]);
+    };
+    const handleRemoveItem = (index) => {
+      const updatedItems = [...items];
+      updatedItems.splice(index, 1);
+      setItems(updatedItems);
+    };
+  
+    return (
       <div>
-        <input
-          type="text"
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-        />
-        <button onClick={handleAddItem}>Agregar</button>
-        <button onClick={handleReset}>Reset</button>
+        <ul>
+          {items.map((item, index) => (
+            <li key={index}>
+              {item}
+              <button onClick={() => handleRemoveItem(index)}>Remove</button>
+            </li>
+          ))}
+        </ul>
+        <div>
+          <input
+            type="text"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+          <button onClick={handleAddItem}>Agregar</button>
+          <button onClick={handleReset}>Reset</button>
+        </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
