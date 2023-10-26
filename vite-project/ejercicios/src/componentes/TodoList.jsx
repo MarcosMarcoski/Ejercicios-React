9491ccbc01509214482acaf4ffce6df7532d94e4
Marcoski <marcosdediego@gmail.com>
2023-10-26 14:01:34 +0200

    ejercicio 57

--- vite-project/ejercicios/src/componentes/TodoList.jsx
+++ vite-project/ejercicios/src/componentes/TodoList.jsx
@@ -1,34 +1,30 @@
 import { useState } from "react";
 
-export function TodoList(){
-    const [items, setItems] = useState([]);
-    const [inputValue, setInputValue] = useState('');
+export function TodoList() {
+  const [items, setItems] = useState([]);
+  const [inputValue, setInputValue] = useState("");
 
-    const handleAddItem = () => {
-      if (inputValue.trim() !== '') {
-        setItems([...items, inputValue]);
-        
-      }
-    };
-    return(
-
-        <div>
-            
-            <ul>
-              {items.map((item, index) => (
-                <li key={index}>{item}</li>
-              ))}
-            </ul>
-            <div>
-              <input
-                type="text"
-                value={inputValue}
-                onChange={(e) => setInputValue(e.target.value)}
-              />
-              <button onClick={handleAddItem}>Agregar</button>
-            </div>
-          </div>
-    )
+  const handleAddItem = () => {
+    if (inputValue.trim() !== "") {
+      setItems([...items, inputValue]);
+      setInputValue("");
+    }
+  };
+  return (
+    <div>
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+      <div>
+        <input
+          type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <button onClick={handleAddItem}>Agregar</button>
+      </div>
+    </div>
+  );
 }
-
-    
