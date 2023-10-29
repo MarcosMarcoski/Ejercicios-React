213e7a6fc0cd54b47711cc68ac84e90a9e11c692
Marcoski <marcosdediego@gmail.com>
2023-10-29 02:35:57 +0100

    ejercicio 79

--- vite-project/ejercicios/src/componentes/GitHubUser.jsx
+++ vite-project/ejercicios/src/componentes/GitHubUser.jsx
@@ -1,13 +1,15 @@
 import useGithubUser from "./UseGitHubUser";
 
 export function GitHubUser({ username }) {
-  const data = useGithubUser(username);
+  const{ data , loading , error }= useGithubUser(username)
   
     
   return (
     <div>
       {data && (
         <div>
+          {loading && <h1>Cargando...</h1>}
+      {error && <h1>Ha habido un error</h1>}
           <h1>{data.name}</h1>
           <h2>{data.login}</h2>
           <img src={data.avatar_url} alt="Avatar" />
