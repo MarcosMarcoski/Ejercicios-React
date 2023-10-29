e3a121f068057179e897cf038ffa87f0e7b6643a
Marcoski <marcosdediego@gmail.com>
2023-10-29 02:28:04 +0100

    ejercicio 78

--- vite-project/ejercicios/src/componentes/UseGitHubUser.jsx
+++ vite-project/ejercicios/src/componentes/UseGitHubUser.jsx
@@ -1,18 +1,20 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-
 export function useGithubUser(username) {
-  const [data, setData] = useState(null);
-
-  function fetchGitHubUser(username) {
-    axios
-      .get(`https://api.github.com/users/${username}`)
-      .then((response) => setData(response.data))
-      .catch((e) => console.error("Error", e));
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError]= useState(null)
+  
+    function fetchGitHubUser(username) {
+      setLoading(true)
+      setError(null)
+      axios
+        .get(`https://api.github.com/users/${username}`)
+        .then((response) => setData(response.data))
+        .catch((e) => setError(e), setData(null))
+        .finally(setLoading(false));
+    }
+    useEffect(() => {
+      fetchGitHubUser(username);
+    }, [username]);
+  
+    return {data , loading, error};
   }
-  useEffect(() => {
-    fetchGitHubUser(username);
-  }, [username]);
-
-  return data;
-}
